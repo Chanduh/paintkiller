c19517cd7b1533aaf2b2794fb03c733bf08774dc
Chanda Gonet <chandagonet@gmail.com>
2022-11-04 17:12:42 -0400

    add products

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const menu = document.getElementById('sidebar')
-const logo = document.getElementById('logo')
 const skull = document.getElementById('skull')
 
 
-logo.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-});
+
 
 skull.addEventListener('click', function() {
     menu.classList.toggle('is-active');
